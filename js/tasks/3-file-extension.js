40d1928ccb39216c2f18122ac35ffe372352be9c
Phani Kalyan <palatlaphanikalyan@gmail.com>
2022-03-02 18:21:41 +1100

    function to extract file extension from a file name

--- js/tasks/3-file-extension.js
+++ js/tasks/3-file-extension.js
@@ -7,4 +7,23 @@
  */
 export default function getFileExtension(filename) {
 
+    //function which takes string as an input and returns the extension considering . as the seperator
+
+    //step 1 finding the index of . in the given string
+    let index_of_period = filename.indexOf('.') //indexOf returns index of . in the string, if not found returns -1
+
+    //step 2 validate if the string has . and doesnot have . as the right most character
+    if ((index_of_period!=-1) && (filename.length> index_of_period+1))
+    {
+        //return right most characters from .
+        return filename.slice(index_of_period+1);
+    }
+    else
+    {
+        return false
+        
+    }
+
+
+
 }
