15d55475300a31e849678fb2b7094e826f3c82a0
Phani Kalyan <palatlaphanikalyan@gmail.com>
2022-03-02 18:34:18 +1100

    function which takes array, and returns sum of integer elements present in all levels of the input array

--- js/tasks/5-array-sum.js
+++ js/tasks/5-array-sum.js
@@ -6,5 +6,22 @@
  * @returns {Number} The sum of all integers contained in the input, at any level.
  */
 export default function arraySum(startOfTree) {
+    //function which takes an array as an input and returns sum of all integers in all nested levels
+    
+    let sum=0 //set sum to 0
 
+    //iterate through the array
+    for (let i = 0; i < startOfTree.length; i++) {
+        if (Array.isArray(startOfTree[i]))//if the current element is an array reuse the same function to set the sum
+        {
+           sum=sum+arraySum(startOfTree[i]);
+        }
+        else if (Number.isInteger(startOfTree[i]))// if current element is integer add to sum
+        {
+            sum=sum+startOfTree[i]
+        }
+   
+   }
+    
+     return sum;   // return sum
 }
